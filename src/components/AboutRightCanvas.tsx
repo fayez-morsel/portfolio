c59a4888aec5064d859861a14d85f4a33fe2cbff
fayez-morsel <fayez.mercel@isae.edu.lb>
2026-07-09 15:49:46 +0300

    retune right canvas motion
    Update the floating crystal and flower layers to match the revised animation set.

--- src/components/AboutRightCanvas.tsx
+++ src/components/AboutRightCanvas.tsx
@@ -6,7 +6,7 @@ export default function AboutRightCanvas() {
   return (
     <div className="w-full h-full relative">
       {/* Crystal / Cubes (top-right) */}
-      <div className="absolute top-[15%] right-[15%] w-[130px] h-[130px] animate-float-1">
+      <div className="absolute top-[15%] right-[15%] w-[130px] h-[130px] animate-float-2">
         <img
           src="/assets/floating_crystal.png?v=4"
           alt="Floating Crystal"
@@ -15,7 +15,7 @@ export default function AboutRightCanvas() {
       </div>
 
       {/* Flower (bottom-right) */}
-      <div className="absolute bottom-[15%] left-[15%] w-[130px] h-[130px] animate-float-2">
+      <div className="absolute bottom-[15%] left-[15%] w-[130px] h-[130px] animate-float-4">
         <img
           src="/assets/floating_flower.png"
           alt="Floating Flower"
